dd96a7bfe9102a27cdcd1cb17a065b66b99608e3
TWA-Coder <Arsene.Karuretwa@mtn.com>
2025-07-13 21:43:14 +0200

    first commit in alx python project

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+
+number1 = 10
+number2 = 5
+add = number1 + number2
+sub = number1 - number2
+mul = number1 * number2
+print('Addition of 10 and 5 is',add )
+print('Subtraction of 10 and 5 is', sub)
+print('Multiplication of 10 and 5 is', mul)

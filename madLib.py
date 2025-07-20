1fa7ed520efd854848c62a02e4f8435df2103c8a
TWA-Coder <twakaru744@gmail.com>
2025-07-20 13:51:42 +0200

    weather advice

---
+++ madLib.py
@@ -0,0 +1,11 @@
+#On a beautiful (adjective) day, I went to the zoo. I saw a funny (adjective) monkey swinging from the trees. 
+# Then, I spotted a majestic (adjective) lion lounging in the sun.  
+# What a wild and (adjective) experience! 
+
+adj1 = input("Is it cold or hot?: ")
+adj2 = input("Is it black or blue?: ")
+adj3 = input("is it male or female?: ")
+adj4 = input("are you happy or sad?: ")
+
+message = f"On a beautiful {adj1} day, I went to the zoo.\n I saw a funny {adj2} monkey swinging from the trees. \nThen, I spotted a majestic {adj3} lion lounging in the sun. \n What a wild and {adj4} experience!"
+print(message)

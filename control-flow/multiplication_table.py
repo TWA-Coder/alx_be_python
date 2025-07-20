e4bef8f295f481c047cdc4caf22809a002c3ff1d
TWA-Coder <twakaru744@gmail.com>
2025-07-20 16:47:12 +0200

    multiplication

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
-list = [1,2,3,4,5,6,7,8,9,10]
+#list = [1,2,3,4,5,6,7,8,9,10]
 
-for n in list:
+for n in range(1, 11):
     print(f"{number} * {n} =", number * n)

3e6645111d7e5d4a5e64ac51f809f4e100f9a8da
TWA-Coder <twakaru744@gmail.com>
2025-08-09 15:10:18 +0200

    arthimetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,19 +1,21 @@
 def perform_operation(num1,num2,operation):
-    if operation == "add":
-        ans = num1 + num2
-        
-    elif operation == "subtract":
-        ans = num1 - num2
-        
-    elif operation == "multiply":
-        ans = num1 * num2
-        
-    elif operation == "divide":
-        if num2 <= 0:
-            ans = "the second number must be greeter than zero"
-        else:
-            ans = num1 / num2
-    else:
-        ans = "Invalid input"
 
+    match operation:
+        case x if x =="add": 
+            ans = num1 + num2
+        case x if x =="subtract":
+            ans = num1 - num2
+
+        case x if x =="multiply":
+            ans = num1 * num2
+
+        case x if x =="divide":
+            if num2 <= 0:
+                ans = "the second number must be greeter than zero"
+            else:
+                ans = num1 / num2
+
+        case _:
+            ans = "Invalid input" 
+        
     return ans

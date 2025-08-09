4e1409c4539c4e00a1ccddab6f8e0991afbad154
TWA-Coder <twakaru744@gmail.com>
2025-08-09 20:35:04 +0200

    fahrenheit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,13 +1,15 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 9/5
-CELSIUS_TO_FAHRENHEIT_FACTOR  = 5/9
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR  = 9/5
 
 def convert_to_celsius(fahrenheit):
-    temp_celsius = float(CELSIUS_TO_FAHRENHEIT_FACTOR * (fahrenheit - 32))
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
+    temp_celsius = float(FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32))
 
     print(f"{fahrenheit}°F is {temp_celsius}°C")
 
 def convert_to_fahrenheit(celsius):
-    temp_fahrenheit = float( FAHRENHEIT_TO_CELSIUS_FACTOR * (celsius + 32))
+    global CELSIUS_TO_FAHRENHEIT_FACTOR 
+    temp_fahrenheit = float( (CELSIUS_TO_FAHRENHEIT_FACTOR  * celsius) + 32)
 
     print(f"{celsius}°C is {temp_fahrenheit}°F")    
 

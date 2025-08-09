d9693078b44bc102ec0cd3db0657d9295f83f0cd
TWA-Coder <twakaru744@gmail.com>
2025-08-09 15:18:25 +0200

    arthimetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,7 +10,9 @@ def perform_operation(num1, num2, operation):
             ans = num1 * num2
 
         case x if x =="divide":
-            if num2 <= 0:
+            if num2 < 0:
+                ans = "the second number must be greeter than zero"
+            elif num2 == 0:
                 ans = "the second number must be greeter than zero"
             else:
                 ans = num1 / num2
